7c52134a89db4fa86123657488c29dd2d6509c7f
Manik Panwar <mpanwar@GHC34.GHC.ANDREW.CMU.EDU>
2016-12-09 17:22:52 -0500

    Got AI working with move change lists to help prevent lots of locking on global struct

--- src/alphabeta.h
+++ src/alphabeta.h
@@ -4,16 +4,24 @@
 #define POSINF INT_MAX     // may need to include <limits.h>
 #define NEGINF INT_MIN
 
-#define MAXDEPTH 10
+#define MAXDEPTH 4
 
 #define MAXI 1
 #define MINI 2
 
 typedef struct minimaxResult* minimaxResult_t;
+typedef struct movesListElem* movesListElem_t;
 
 struct minimaxResult {
   int bestRes;
   move_t move;
+};
+
+struct movesListElem {
+    move_t m;
+    int player;
+    board_piece startPiece;
+    board_piece endPiece;
 };
 
 move_t nextMove(board_t board, int aiType, int curPlayer);
